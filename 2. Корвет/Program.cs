51e16e8361e4221fd16236a26b23df66210da36b
Vcyprus32@mail.ru <Vcyprus32@mail.ru>
2022-12-16 01:19:18 +0200

    Добавьте файлы проекта.

---
+++ 2. Корвет/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int N, A, B;
+Console.Write("n=");
+N = int.Parse(Console.ReadLine());
+Console.Write("a=");
+A = int.Parse(Console.ReadLine());
+Console.Write("b=");
+B = int.Parse(Console.ReadLine());
+
+int P = (N * A * B) * 2;
+Console.WriteLine($" = {P}"); 
+
